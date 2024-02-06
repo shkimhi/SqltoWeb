2f05b1c8cbe26ad2719645f63f6cc6c01292ee54
shkimhi <ajfqhksh1@gmail.com>
2024-02-06 23:37:03 +0900

    init

---
+++ src/main/java/com/sh/sqltoweb/SqltoWebApplication.java
@@ -0,0 +1,13 @@
+package com.sh.sqltoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqltoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqltoWebApplication.class, args);
+    }
+
+}
